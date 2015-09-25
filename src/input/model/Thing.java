e55e05bb0ff7962091cc544927a6784b8cce2ede
Sydothekido11 <sydnemelka@gmail.com>
2015-09-25 12:03:44 -0600

    Named some GUI.

--- src/input/model/Thing.java
+++ src/input/model/Thing.java
@@ -2,5 +2,22 @@ package input.model;
 
 public class Thing
 {
-
+	private String name;
+	private int age;
+	private double weight;
+	
+	public Thing()
+	{
+		age = 22;
+		weight = 150;
+		name = "";
+	}
+	
+	public Thing (String name, int age, double weight)
+	{
+		this.age = age;
+		this.name = name;
+		this.weight = weight;
+		
+	}
 }
